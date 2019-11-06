b63aa65d4de4a0e59827e03b8242aac98bff1179
Grabner <andreas.grabner@dynatrace.com>
2019-11-06 12:20:31 +0100

    updated charts

--- keptn/add_resources.sh
+++ keptn/add_resources.sh
@@ -9,10 +9,10 @@ echo "Adding resource files to our repo"
 keptn add-resource --project=keptn05eks --service=simplenode --stage=staging --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
 keptn add-resource --project=keptn05eks --service=simplenode --stage=staging --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 keptn add-resource --project=keptn05eks --service=simplenode --stage=staging --resource=qualitygates/service-indicators.yaml --resourceUri=service-indicators.yaml
-keptn add-resource --project=keptn05eks --service=simplenode --stage=staging --resource=service-objectives.yaml --resourceUri=service-objectives.yaml
+keptn add-resource --project=keptn05eks --service=simplenode --stage=staging --resource=qualitygates/service-objectives.yaml --resourceUri=service-objectives.yaml
 
 # adding resources for production
 keptn add-resource --project=keptn05eks --service=simplenode --stage=prod --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
 keptn add-resource --project=keptn05eks --service=simplenode --stage=prod --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 keptn add-resource --project=keptn05eks --service=simplenode --stage=prod --resource=qualitygates/service-indicators.yaml --resourceUri=service-indicators.yaml
-keptn add-resource --project=keptn05eks --service=simplenode --stage=prod --resource=service-objectives.yaml --resourceUri=service-objectives.yaml
+keptn add-resource --project=keptn05eks --service=simplenode --stage=prod --resource=qualitygates/service-objectives.yaml --resourceUri=service-objectives.yaml
