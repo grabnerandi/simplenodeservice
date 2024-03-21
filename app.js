06d35f9019aebeec239c2df45c26e7f126f4f225
Adam Gardner <adam@agardner.net>
2024-03-21 15:59:09 +0100

    update to use in-process

--- app.js
+++ app.js
@@ -16,13 +16,11 @@ var port = process.env.PORT || 8080,
     keptn_project = process.env.KEPTN_PROJECT || EMPTY,
     keptn_stage = process.env.KEPTN_STAGE || EMPTY,
     keptn_service = process.env.KEPTN_SERVICE || EMPTY,
-    html = fs.readFileSync('index.html').toString().replace("HOSTNAME", os.hostname()), //  + " with DT_TAGS=" + dttags + "\nDT_CUSTOM_PROP=" + dtcustprops + "\nDT_CLUSTER_ID=" + dtclusterid);
-    flagd_host = process.env.FEATURE_FLAG_HOST || "127.0.0.1",
-    flagd_port = process.env.FEATURE_FLAG_PORT || 8013;
+    html = fs.readFileSync('index.html').toString().replace("HOSTNAME", os.hostname()); //  + " with DT_TAGS=" + dttags + "\nDT_CUSTOM_PROP=" + dtcustprops + "\nDT_CLUSTER_ID=" + dtclusterid);
 
 // OpenFeature Initialisation Code
 // Register your feature flag provider
-OpenFeature.setProvider(new FlagdProvider({host: flagd_host, port: flagd_port}));
+OpenFeature.setProvider(new FlagdProvider());
 // create a new client
 const client = OpenFeature.getClient();
 
